a6f362e65d1121fa160f81ce69be018c79fe21e1
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-16 12:01:11 -0700

    Condense objects into constructors.

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,12 @@ function Dog(name, colors, age){
   this.name = name,
   this.colors = colors,
   this.age = age
-};
+}
 
+Dog.prototype.speak = function() {
+  console.log("Woof!")
+}
 
-var dog1 = {
-  name: "Falcor",
-  colors: ["black"],
-  age: 4,
-};
-
-var dog2 {
-  name: "Nola",
-  colors: ["white, black"],
-  age: 6,
-};
-
-var dog3 {
-  name: "Patsy",
-  colors: ["brown"],
-  age: 7,
-};
+Dog.prototype.humanYears = function() {
+  return this.age * 7
+}
