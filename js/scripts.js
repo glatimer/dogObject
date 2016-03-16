ece3718110a5c670e8b420e407d62ee14cbd5039
Genevieve Latimer <genevievelatimer@gmail.com>
2016-03-16 11:01:35 -0700

    Use constructors to store information.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function Dog(name, colors, age){
+  this.name = name,
+  this.colors = colors,
+  this.age = age
+};
+
+
+var dog1 = {
+  name: "Falcor",
+  colors: ["black"],
+  age: 4,
+};
+
+var dog2 {
+  name: "Nola",
+  colors: ["white, black"],
+  age: 6,
+};
+
+var dog3 {
+  name: "Patsy",
+  colors: ["brown"],
+  age: 7,
+};
